96246adb42bfcd2717a0953fa1ca01b758ccab5f
ahmed-lotfy-dev <elshenawy19@gmail.com>
2026-02-02 16:11:26 +0200

    patch security vulenrability of aws-sdk depend on fast-xml-parser with security vulenrability to apatched version

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -7,7 +7,6 @@ const globalForPrisma = global as unknown as {
 };
 
 const url = process.env.DATABASE_URL;
-console.log(`📡 Prisma initializing with URL: ${url?.split('@')[1] || 'MISSING'}`); // Log only suffix for safety
 
 const pool = new Pool({
   connectionString: url,
